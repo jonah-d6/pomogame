c92afa6264a6ec64576a128db31d2ba1d85e0f69
jonah-d6 <jonah.dean@maine.edu>
2024-11-09 12:20:21 -0500

    change it to be using a loop instead of recursion

--- clock.py
+++ clock.py
@@ -5,14 +5,13 @@ root = Tk()
 C = Canvas(root, bg="white", height=600, width=600)
 
 def anotherArc(i):
-    if (i > 100):
-        return
     arc = C.create_arc(180, 150, 80, 210, start = i, extent = (i+1),
         fill = "green")
     root.update()
-    root.after(100, anotherArc(i+1))
 
-anotherArc(0)
+for i in range(100):
+    root.after(1000, anotherArc(i))
+
 
 C.pack()
 mainloop()
