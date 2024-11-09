6e44d86daacc6a5e7c44a876d7fe9dc10fc6db81
Aragon9723 <160268275+Aragon9723@users.noreply.github.com>
2024-11-09 13:49:39 -0500

    Update stuff.py

--- stuff.py
+++ stuff.py
@@ -1,17 +1,8 @@
-import tkinter as tk
-import time
+import math
 
-root = tk.Tk()
-canvas = tk.Canvas(root, width=200, height=200)
-canvas.pack()
+currentExperience = 600
+currentLevel = int(math.log(currentExperience))
+leftoverExperince = currentExperience - math.e ** currentLevel
 
-def createArc(i):
-    canvas.create_arc(10, 10, 190, 190, start=0, extent=i*5, fill='blue')
+print(currentExperience,currentLevel,leftoverExperince)
 
-
-for i in range(360):
-    createArc(i)
-    canvas.update()
-    time.sleep(1)
-    
-root.mainloop()
