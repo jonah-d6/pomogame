ad7ff47070746094ffb13d9a049b45b6b7cca2d0
jonah-d6 <jonah.dean@maine.edu>
2024-11-09 11:35:30 -0500

    add test gui

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import tkinter as tk
+
 def main():
-    return False
+    root = tk.Tk()
+
+    label = tk.Label(root, text='We cookin now')
+    label.pack()
+
+    root.mainloop()
 
 if __name__ == '__main__':
     main()
