75fcb20b1de59a949f3016db18cec2336925dc76
jonah-d6 <jonah.dean@maine.edu>
2024-11-09 12:10:45 -0500

    actually call the function and make a condition to exit the recursion

--- clock.py
+++ clock.py
@@ -4,10 +4,15 @@ root = Tk()
 
 C = Canvas(root, bg="white", height=600, width=600)
 
-i = 0 #counter for degrees
 def anotherArc(i):
+    if (i > 100):
+        return
     arc = C.create_arc(180, 150, 80, 210, start = i, extent = (i+1),
         fill = "green")
+    root.update()
     root.after(100, anotherArc(i+1))
+
+anotherArc(0)
+
 C.pack()
 mainloop()
